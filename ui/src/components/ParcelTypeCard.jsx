ee225481431b8ec7a1db5be7fd9789b38093b99b
Accir <acciras@gmail.com>
2021-04-01 21:14:05 +0300

    DDD-20 main page ui fix

--- ui/src/components/ParcelTypeCard.jsx
+++ ui/src/components/ParcelTypeCard.jsx
@@ -5,7 +5,7 @@ import ButtonBase from "@material-ui/core/ButtonBase";
 function ParcelTypeCard(props) {
   return (
     <ButtonBase
-      className={props.name === props.selectedPaymentType ? "selected-card w-100" : "w-100 scale-down"}
+      className={props.name === props.selectedPackageType ? "selected-card w-100" : "w-100 scale-down"}
       onClick={() => props.onClick(props.name)}>
       <div className="form-inner form-inner-document w-100">
         <div className="form-header">{props.name}</div>
