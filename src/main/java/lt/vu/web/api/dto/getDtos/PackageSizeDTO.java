1ac126af7799862346d9bfe3eebcf66b81da0c72
Skullcr1 <kornelismak@gmail.com>
2021-04-03 13:50:29 +0300

    DDD-23 Move get DTOs to getDTOs folder

---
+++ src/main/java/lt/vu/web/api/dto/getDtos/PackageSizeDTO.java
@@ -0,0 +1,44 @@
+package lt.vu.web.api.dto.getDtos;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.persistence.orm.entities.PackageSize;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter @Setter
+public class PackageSizeDTO {
+
+    private int id;
+
+    private String title;
+
+    private int maxWeight;
+
+    private int length;
+
+    private int height;
+
+    private int width;
+
+    public static PackageSizeDTO createFromEntity(PackageSize packageSize) {
+        PackageSizeDTO dto = new PackageSizeDTO();
+
+        dto.setId(packageSize.getId());
+        dto.setTitle(packageSize.getTitle());
+        dto.setMaxWeight(packageSize.getMaxWeight());
+        dto.setLength(packageSize.getLength());
+        dto.setHeight(packageSize.getHeight());
+        dto.setWidth(packageSize.getWidth());
+
+        return dto;
+    }
+
+    public static List<PackageSizeDTO> createMany(List<PackageSize> packageSizes) {
+        return packageSizes
+                .stream()
+                .map(PackageSizeDTO::createFromEntity)
+                .collect(Collectors.toList());
+    }
+}
