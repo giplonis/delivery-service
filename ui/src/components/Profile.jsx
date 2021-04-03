92c0f560e2af6dc1303c2969c08dc9ddb23d841f
Accir <acciras@gmail.com>
2021-04-04 02:56:25 +0300

    DDD-45 add personal data modification form

--- ui/src/components/Profile.jsx
+++ ui/src/components/Profile.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
+import PersonalData from "./PersonalData";
 
 function Profile() {
   return (
     <Container>
       <div className="content-wrapper">
         <Header />
-        {/* Content Here */}
+        <Grid container spacing={9}>
+          <Grid item xs={6}>
+            <PersonalData />
+          </Grid>
+          <Grid item xs={6}></Grid>
+        </Grid>
       </div>
       <Footer />
     </Container>
