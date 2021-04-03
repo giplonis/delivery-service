470ba6bbe84a0409a23a6f6db6f792abc3cd69b8
Gintautas Plonis <gplonis@gmail.com>
2021-04-03 12:13:10 +0300

    DDD-22 Install official Swagger packages, create documentation endpoint

---
+++ src/main/java/lt/vu/web/api/v1/controller/packageSize/ListPackageSizeController.java
@@ -0,0 +1,48 @@
+package lt.vu.web.api.v1.controller.packageSize;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lt.vu.persistence.orm.entities.PackageSize;
+import lt.vu.persistence.orm.repository.PackageSizeRepository;
+import lt.vu.web.api.v1.dto.packageSize.ListPackageSizeDTO;
+import lt.vu.web.api.v1.dto.packageSize.PackageSizeDTO;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/package-size")
+@RequestScoped
+public class ListPackageSizeController {
+
+    @Inject
+    private PackageSizeRepository packageSizeRepository;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary = "Fetch list of package sizes available",
+        tags = { "PackageSize" },
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                content = @Content(schema = @Schema(implementation = ListPackageSizeDTO.class))
+            )
+        }
+    )
+    public Response listAction() {
+        List<PackageSize> packageSizes = this.packageSizeRepository.findAll();
+
+        return Response
+                .ok(new ListPackageSizeDTO(PackageSizeDTO.createMany(packageSizes)))
+                .build();
+    }
+}
---
+++ src/main/java/lt/vu/web/api/v1/dto/packageSize/PackageSizeDTO.java
@@ -0,0 +1,44 @@
+package lt.vu.web.api.v1.dto.packageSize;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.persistence.orm.entities.PackageSize;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter @Setter
+public class PackageSizeDTO {
+
+    private int id;
+
+    private String title;
+
+    private int maxWeight;
+
+    private int length;
+
+    private int height;
+
+    private int width;
+
+    public static PackageSizeDTO createFromEntity(PackageSize packageSize) {
+        PackageSizeDTO dto = new PackageSizeDTO();
+
+        dto.setId(packageSize.getId());
+        dto.setTitle(packageSize.getTitle());
+        dto.setMaxWeight(packageSize.getMaxWeight());
+        dto.setLength(packageSize.getLength());
+        dto.setHeight(packageSize.getHeight());
+        dto.setWidth(packageSize.getWidth());
+
+        return dto;
+    }
+
+    public static List<PackageSizeDTO> createMany(List<PackageSize> packageSizes) {
+        return packageSizes
+                .stream()
+                .map(PackageSizeDTO::createFromEntity)
+                .collect(Collectors.toList());
+    }
+}
