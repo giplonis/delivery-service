774416da0c57b859eba13c6219a47393b0801b53
Skullcr1 <kornelismak@gmail.com>
2021-04-04 23:42:15 +0300

    DDD-23 Persist order inside controller

--- src/main/java/lt/vu/web/api/controller/order/PostOrderController.java
+++ src/main/java/lt/vu/web/api/controller/order/PostOrderController.java
@@ -1,11 +1,13 @@
 package lt.vu.web.api.controller.order;
 
 import lt.vu.persistence.orm.entities.Order;
-import lt.vu.web.api.domain.order.CreateOrder;
+import lt.vu.persistence.orm.repository.OrderRepository;
+import lt.vu.web.api.factory.OrderFactory;
 import lt.vu.web.api.dto.postDtos.order.OrderDTO;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,14 +17,18 @@ import javax.ws.rs.core.Response;
 public class PostOrderController {
 
     @Inject
-    private CreateOrder create;
+    private OrderFactory orderFactory;
+
+    @Inject
+    private OrderRepository orderRepository;
 
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
     public Response createAction(OrderDTO orderDTO){
-        Order order = create.order(orderDTO);
-
+        Order order = orderFactory.create(orderDTO);
+        orderRepository.persist(order);
         return Response.ok(order, MediaType.APPLICATION_JSON).build();
     }
 }
