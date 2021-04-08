1f432f10956e3ede33fb1955edf7a18b3bc14109
Zygis0321 <zygimantas.remeika@gmail.com>
2021-04-08 16:27:40 +0300

    returned deleted files

---
+++ src/main/java/lt/vu/infrastructure/interceptors/MethodLogger.java
@@ -0,0 +1,17 @@
+package lt.vu.infrastructure.interceptors;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.io.Serializable;
+
+@Interceptor
+@LoggedInvocation
+public class MethodLogger implements Serializable{
+    @AroundInvoke
+    public Object logMethodInvocation(InvocationContext context) throws Exception {
+        System.out.println("Called method: " + context.getMethod().getName());
+
+        return context.proceed();
+    }
+}
