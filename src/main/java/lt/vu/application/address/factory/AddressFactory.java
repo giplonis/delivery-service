a1d0743a5f8fd1954d2075227846e178b7f04cbd
Gintautas Plonis <gplonis@gmail.com>
2021-05-01 12:25:48 +0300

    DDD-56 Move factories under domain namespaces

--- src/main/java/lt/vu/application/address/factory/AddressFactory.java
+++ src/main/java/lt/vu/application/address/factory/AddressFactory.java
@@ -1,4 +1,4 @@
-package lt.vu.web.api.v1.factory;
+package lt.vu.application.address.factory;
 
 import lt.vu.persistence.orm.entities.Address;
 import lt.vu.web.api.v1.dto.address.PostAddressDTO;
@@ -8,7 +8,7 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class AddressFactory {
 
-    public Address create(PostAddressDTO addressDTO) {
+    public Address createFromDTO(PostAddressDTO addressDTO) {
         Address address = new Address();
 
         address.setCity(addressDTO.getCity());
--- src/main/java/lt/vu/application/order/factory/OrderFactory.java
+++ src/main/java/lt/vu/application/order/factory/OrderFactory.java
@@ -5,7 +5,7 @@ import lt.vu.persistence.orm.entities.*;
 import lt.vu.persistence.orm.repository.AttributeRepository;
 import lt.vu.persistence.orm.repository.PackageOptionRepository;
 import lt.vu.web.api.v1.dto.order.PostOrderDTO;
-import lt.vu.web.api.v1.factory.UserInfoFactory;
+import lt.vu.application.userInfo.factory.UserInfoFactory;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -30,8 +30,8 @@ public class OrderFactory {
 
         PackageOption packageOption = this.packageOptionRepository.findOneById(orderDTO.getPackageOptionId());
 
-        order.setSenderInfo(this.userInfoFactory.create(orderDTO.getSender()));
-        order.setRecipientInfo(this.userInfoFactory.create(orderDTO.getRecipient()));
+        order.setSenderInfo(this.userInfoFactory.createFromDTO(orderDTO.getSender()));
+        order.setRecipientInfo(this.userInfoFactory.createFromDTO(orderDTO.getRecipient()));
         order.setStatus(OrderStatus.NEW);
         order.setPickupDateTime(orderDTO.getPickUpDate());
         order.setPackageOption(packageOption);
