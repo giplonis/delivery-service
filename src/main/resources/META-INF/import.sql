aa7273ba4057aff12bacac9340c91fd13e18eded
Gintautas Plonis <gplonis@gmail.com>
2021-05-25 08:19:14 +0300

    DDD-102 Implement optimistic locking

--- src/main/resources/META-INF/import.sql
+++ src/main/resources/META-INF/import.sql
@@ -31,7 +31,7 @@ insert into ATTRIBUTE (ID, LABEL, TYPE, ADDITIONAL_PRICE) values (1, 'Fragile', 
 insert into USER_INFO (ID, FIRST_NAME, LAST_NAME, ADDRESS_ID, EMAIL, PHONE_NUMBER) values (1, 'Jonas', 'Jonauskas', 1, 'jonas@gmail.com', 862594785);
 
 -- Orders
-insert into `ORDER` (ID, CREATED_AT, STATUS, PICKUP_DATE_TIME, TOTAL_PRICE, PACKAGE_OPTION_ID, SENDER_INFO_ID, RECIPIENT_INFO_ID, USER_ID) values (1, current_timestamp(), 0, dateadd('DAY', 2, current_timestamp()), 42069, 1, 1, 1, null);
+insert into `ORDER` (ID, CREATED_AT, STATUS, PICKUP_DATE_TIME, TOTAL_PRICE, PACKAGE_OPTION_ID, SENDER_INFO_ID, RECIPIENT_INFO_ID, USER_ID, VERSION) values (1, current_timestamp(), 0, dateadd('DAY', 2, current_timestamp()), 42069, 1, 1, 1, null, 0);
 
 -- Users
 -- PASSWORD = password
